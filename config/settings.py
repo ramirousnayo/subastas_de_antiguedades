a93977e540ac6bc61eea0427d6e931870a50680b
Ramiro Usnayo <rusnayo22@gmail.com>
2026-03-16 15:12:06 -0300

    Final phase: Advanced search, category filters, user dashboard, and premium UX enhancements

--- config/settings.py
+++ config/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -66,6 +66,9 @@ TEMPLATES = [
         },
     },
 ]
+
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home'
 
 WSGI_APPLICATION = 'config.wsgi.application'
 
